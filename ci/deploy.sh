13e30b1f4b61535f63acc70887ed0e7cf86726dd
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-10-21 10:05:11 +0100

    Start on CI and chartpress

---
+++ ci/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+# Decrypt a private SSH key having its public key registered on GitHub. It will
+# be used to establish an identity with rights to push to the repo hosting our
+# Helm charts: https://github.com/dask/helm-chart
+openssl aes-256-cbc -K $TODO -iv $TODO -in ci/id_rsa.enc -out ci/id_rsa -d
+chmod 0400 ci/id_rsa
+
+# Activate logging of bash commands now that the sensitive stuff is done
+set -x
+
+# As chartpress uses git to push to our Helm chart repository, we configure
+# git ahead of time to use the identity we decrypted earlier.
+export GIT_SSH_COMMAND="ssh -i ${PWD}/ci/id_rsa"
+
+chartpress --commit-range "${TRAVIS_COMMIT_RANGE}" --tag "${TRAVIS_TAG}" --publish-chart
