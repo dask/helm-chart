a0b468a51e54040407ad84017ad716719206b820
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-08-26 09:53:44 -0500

    Update release infrastructure
    * Use chartpress to set version
    * Set appVersion for DaskHub

--- ci/release.sh
+++ ci/release.sh
@@ -3,14 +3,9 @@
 set -eu
 
 VERSION=$1
-CHART_PATH=dask/Chart.yaml
-
-# Replace version in Chart.yaml
-sed -i.bak "s/^version:.*/version: $VERSION/" $CHART_PATH
-rm $CHART_PATH.bak
 
 # Commit Chart.yaml
-git commit -a -m "Bump version to $VERSION"
+git commit --allow-empty -m "Version $VERSION"
 
 # Git tag new version
 git tag -a $VERSION -m "Version $VERSION"
